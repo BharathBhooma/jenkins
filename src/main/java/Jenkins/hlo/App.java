73dba3901012126a641787bc1d2bbe65889f5de6
Bhooma Bharath (Tata Consultancy Services Limi) <v-bbharath@microsoft.com>
2025-12-25 13:12:26 +0530

    rename

--- src/main/java/Jenkins/hlo/App.java
+++ src/main/java/Jenkins/hlo/App.java
@@ -1,7 +1,10 @@
 package Jenkins.hlo;
 
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+	  System.setProperty("webdriver.edge.driver", "C:\\Users\\v-bbharath\\Downloads\\edgedriver_win64 (2)\\msedgedriver.exe"); 
+    EdgeDriver driver = new EdgeDriver();
+    driver.get("https://google.com");  }
 }
