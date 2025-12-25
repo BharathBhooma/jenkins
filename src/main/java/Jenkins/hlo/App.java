e0692a7505628999f0a505c8d0d1b3b83e242adb
Bhooma Bharath (Tata Consultancy Services Limi) <v-bbharath@microsoft.com>
2025-12-25 11:42:55 +0530

    jenkins

---
+++ src/main/java/Jenkins/hlo/App.java
@@ -0,0 +1,7 @@
+package Jenkins.hlo;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Jenkins/hlo/output.java
@@ -0,0 +1,15 @@
+package Jenkins.hlo;
+
+public class output {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a []= {1,1,2,3,3,3};
+		for (int i=0;i<a.length+1;i++) {
+			System.out.println(a[i]);
+		}
+		
+	}
+
+}
